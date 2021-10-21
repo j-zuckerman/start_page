98f48f440686cfc8d46172b5305179dcd3d6121e
j-zuckerman <jameszuckerman7@gmail.com>
2021-10-20 18:30:41 -0700

    Fix bookmark color styling

--- components/bookmark/index.js
+++ components/bookmark/index.js
@@ -3,15 +3,12 @@ import 'remixicon/fonts/remixicon.css';
 import styles from './bookmark.module.css';
 
 function Bookmark({ bookmark }) {
-  useEffect(() => {
-    document.documentElement.style.setProperty(
-      '--bookmark-color',
-      bookmark.color
-    );
-  }, []);
-
   return (
-    <a href={bookmark.link} className={styles.bookmark}>
+    <a
+      href={bookmark.link}
+      className={styles.bookmark}
+      style={{ '--bookmark-color': bookmark.color }}
+    >
       <i className={`${styles.icon} ${bookmark.icon}`}></i>
       <label>
         <span>{bookmark.name}</span>
